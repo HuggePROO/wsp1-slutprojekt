21a22ae0c0829b5fc52bed5d8faf15630209ee26
HuggePROO <hugo.larsson2@elev.ga.ntig.se>
2026-03-09 10:38:55 +0100

    Update Fix

--- app.rb
+++ app.rb
@@ -1,23 +1,157 @@
 require 'debug'
-require "awesome_print"
+require 'sinatra'
+require 'securerandom'
+require 'bcrypt'
 
 class App < Sinatra::Base
+  
+  def db
+    return @db if @db
 
-    setup_development_features(self)
+    @db = SQLite3::Database.new("db/sqlite.db")
+    @db.results_as_hash = true
 
-    # Funktion för att prata med databasen
-    # Exempel på användning: db.execute('SELECT * FROM fruits')
-    def db
-      return @db if @db
-      @db = SQLite3::Database.new(DB_PATH)
-      @db.results_as_hash = true
+    return @db
+  end
 
-      return @db
+  configure do
+    enable :sessions
+    set :session_secret, SecureRandom.hex(64)
+  end
+
+  before do
+    if session[:user_id]
+      @current_user = db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first
+      ap @current_user
+    end
+  end
+
+  # Routen /
+  get '/' do
+    redirect(:"/movies")
+  end
+
+  get '/movies' do
+    @movies = db.execute('SELECT * FROM movies')
+    p @movies
+    erb(:"movies/index")
+  end
+  
+
+  get '/movies/new' do
+    erb(:"movies/new")
+  end
+
+  post '/movies' do
+  db.execute(
+    "INSERT INTO movies (name, poster, runtime, imdb) VALUES (?, ?, ?, ?)",
+    [params[:name], params[:poster], params[:runtime], params[:imdb]]
+  )
+
+  redirect '/'
+end
+
+  get '/movies/:id' do | id |
+    @movie = db.execute('SELECT * FROM movies WHERE id=?',id).first
+    erb(:"movies/show")
+  end
+
+  post '/movies/:id/delete' do | id |
+    db.execute("DELETE FROM movies WHERE id =?", id)
+    redirect("/")
+  end
+ 
+  get '/movies/:id/edit' do | id |
+    @movie = db.execute('SELECT * FROM movies WHERE id=?',id).first
+    erb(:"movies/edit")
+  end
+
+  post "/movies/:id/update" do | id |
+    db.execute("UPDATE movies SET name=?, poster=?, runtime=?, imdb=? WHERE id=?", params.values)
+    redirect("/")
+  end
+
+  get '/acces_denied' do
+    erb(:"movies/acces_denied")
+  end
+
+  get '/login' do
+    erb(:"movies/login")
+  end
+
+  post '/login' do
+    request_username = params[:username]
+    request_plain_password = params[:password]
+
+    user = db.execute("SELECT *
+            FROM users
+            WHERE username = ?",
+            request_username).first
+
+    unless user
+      ap "/login : Invalid username."
+      status 401
+      redirect '/acces_denied'
     end
 
-    # Routen /
-    get '/' do
-        erb :index
-    end
+    db_id = user["id"].to_i
+    db_password_hashed = user["password"].to_s
 
+    # Create a BCrypt object from the hashed password from db
+    bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+    # Check if the plain password matches the hashed password from db
+    if bcrypt_db_password == request_plain_password
+      ap "/login : Logged in -> redirecting to admin"
+      session[:user_id] = db_id
+      redirect '/'
+    else
+      ap "/login : Invalid password."
+      status 401
+      redirect '/acces_denied'
+    end
+  end
+
+  post '/logout' do
+    ap "Logging out"
+    session.clear
+    redirect '/'
+  end
+
+  get '/users/new' do
+    erb(:"movies/users/new")
+  end
+
+  post '/users' do
+    username = params[:username]
+    password = params[:password]
+
+    password_hash = BCrypt::Password.create(password)
+
+    db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, password_hash])
+
+    redirect '/login'
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  post '/saves' do
+  user_id = session[:user_id]
+  movie_id = params[:movie_id]
+  status = params[:status]
+  rating = params[:rating]
+
+  db.execute("INSERT INTO saves (user_id, movie_id, status, rating) VALUES (?, ?, ?, ?)", [user_id, movie_id, status, rating])
+
+  redirect "/movies/#{movie_id}"
+end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,92 @@
 require 'sqlite3'
+require_relative '../config'
+require 'bcrypt'
 
 class Seeder
 
   def self.seed!
-    p "doit"
+    puts "Using db file: #{DB_PATH}"
+    puts "🧹 Dropping old tables..."
+    drop_tables
+    puts "🧱 Creating tables..."
+    create_tables
+    puts "🍎 Populating tables..."
+    populate_tables
+    puts "✅ Done seeding the database!"
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS saves')
+    db.execute('DROP TABLE IF EXISTS users')
+    db.execute('DROP TABLE IF EXISTS movies')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE movies (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      poster TEXT NOT NULL,
+      runtime TEXT NOT NULL,
+      imdb TEXT NOT NULL
+    )')
+
+    db.execute('CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL,
+      password TEXT NOT NULL
+    )')
+
+    db.execute('CREATE TABLE saves (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id INTEGER,
+      movie_id INTEGER,
+      status TEXT NOT NULL,
+      rating INTEGER,
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (movie_id) REFERENCES movies(id)
+    )')
+  end
+
+  def self.populate_tables
+  # Hash the password and convert to string explicitly
+  password_hashed = BCrypt::Password.create("123").to_s
+  puts "Storing hashed password (#{password_hashed}) to DB. Clear text password (123) never saved."
+
+  # Insert the user
+  db.execute('INSERT INTO users (username, password) VALUES (?, ?)', ["hugo", password_hashed])
+
+  # Prepare movies to insert
+  movies = [
+    ["Star Wars Revenge of the Sith", "https://storage.googleapis.com/pod_public/1300/266305.jpg", "2h 20m", "8,8"],
+    ["The Lord of the Rings: The Return of the King", "https://static.posters.cz/image/1300/133052.jpg", "3h 21m", "9,0"],
+    ["Inception", "https://m.media-amazon.com/images/I/91b3Xtjt0IL._AC_UF1000,1000_QL80_.jpg", "2h 28m", "8,8"],
+    ["The Matrix", "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg", "2h 16m", "8,7"],
+    ["Interstellar", "https://m.media-amazon.com/images/I/61ASebTsLpL._AC_UF894,1000_QL80_.jpg", "2h 49m", "8,6"],
+    ["Avatar", "https://i.pinimg.com/736x/8b/2f/a6/8b2fa6fb94810cd0d335b479896f7fc8.jpg", "2h 42m", "7,9"],
+    ["Guardians of the Galaxy", "https://m.media-amazon.com/images/I/81YZ8slCyuL.jpg", "2h 1m", "8,0"],
+    ["Mad Max: Fury Road", "https://m.media-amazon.com/images/I/71rs1WWtoBL._AC_UF1000,1000_QL80_.jpg", "2h 0m", "8,1"],
+    ["Dune: Part One", "https://i0.wp.com/schicksalgemeinschaft.wordpress.com/wp-content/uploads/2021/09/dune-part-one-poster.jpg?fit=810%2C1200&ssl=1", "2h 35m", "8,0"],
+    ["Pirates of the Caribbean: The Curse of the Black Pearl", "https://m.media-amazon.com/images/I/71zji3aER6L.jpg", "2h 23m", "8,1"]
+  ]
+
+  # Insert movies safely
+  movies.each do |name, poster, runtime, imdb|
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES (?, ?, ?, ?)', [name, poster, runtime, imdb])
+  end
+
+  puts "Movies and user successfully populated!"
+end
+
+  private
+
+  def self.db
+    @db ||= begin
+      db = SQLite3::Database.new(DB_PATH)
+      db.results_as_hash = true
+      db
+    end
   end
 
 end
+
+Seeder.seed!
